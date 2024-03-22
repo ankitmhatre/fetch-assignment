a8c33b3af856223bf595d380cf57d485c9e31fed
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2024-03-21 20:40:42 -0400

    create meal detais view

---
+++ mealdb/MealDetailsView.swift
@@ -0,0 +1,24 @@
+//
+//  MealDetailsView.swift
+//  mealdb
+//
+//  Created by Ankit Mhatre on 3/21/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+struct MealDetailView: View {
+    
+    
+    var body: some View {
+        
+        
+        VStack {
+            Text("Meal Details")
+        }
+    }
+    
+}
